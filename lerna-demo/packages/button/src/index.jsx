a02edf9699ecd989d5cf2071906f90988346b7fd
Zico Deng <zicodeng@gmail.com>
2018-08-20 15:22:31 -0700

    Fix Button component render method

--- lerna-demo/packages/button/src/index.jsx
+++ lerna-demo/packages/button/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Button extends React.Component {
     render() {
-        <button onClick={this.handleClickButton}>Hello</button>;
+        return <button onClick={this.handleClickButton}>Hello</button>;
     }
 
     handleClickButton = () => {
